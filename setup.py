26eb3c9e1d032c202a44579f04ed13c0e435a6c1
Todd Sifleet <todd.sifleet@gmail.com>
2014-10-05 20:20:49 -0400

    add packages to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.01',
     author='Todd Siflet',
     author_email='todd.siflet@gmail.com',
-    py_modules=['staticpy.staticpy'],
+    packages=['staticpy'],
     entry_points={
         'console_scripts': [
             'staticpy = staticpy.staticpy:parse_args_and_run'
